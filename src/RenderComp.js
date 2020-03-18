f6a9d368e2b02ca61e398ab3f6f9921d88e6022e
Aditya Rohilla <adicool2294@gmail.com>
2020-03-17 17:12:10 -0700

    small fixes

--- src/RenderComp.js
+++ src/RenderComp.js
@@ -240,25 +240,25 @@ class RenderComp extends Component {
     handleCardSentimentPositive(){
       if(this.state.cardSentimentPositive == true)
       this.setState({cardSentimentPositive: false});
-      else 
+      else
       this.setState({cardSentimentPositive: true});
     }
     handleCardSentimentNegative(){
       if(this.state.cardSentimentNegative == true)
       this.setState({cardSentimentNegative: false});
-      else 
+      else
       this.setState({cardSentimentNegative: true});
     }
     handleInsuranceSentimentPositive(){
       if(this.state.insuranceSentimentPositive == true)
       this.setState({insuranceSentimentPositive: false});
-      else 
+      else
       this.setState({insuranceSentimentPositive: true});
     }
     handleInsuranceSentimentNegative(){
       if(this.state.insuranceSentimentNegative == true)
       this.setState({insuranceSentimentNegative: false})
-      else 
+      else
       this.setState({insuranceSentimentNegative: true})
     }
 
@@ -702,18 +702,18 @@ class RenderComp extends Component {
                     </Grid.Row>
                 <Grid.Row>
                     <Grid.Column width={8}>
-                      <h3>Card/Customer Tweets</h3>
-                      <Checkbox label="Remove Positive Sentiments"  onChange={this.handleCardSentimentPositive}></Checkbox>
-                      <Checkbox label="Remove Negative Sentiments"  onChange={this.handleCardSentimentNegative}></Checkbox>
+                      <h3>Customer Related Messages</h3>
+                      <Checkbox label="Neg" style={{marginRight:"10px"}} onChange={this.handleCardSentimentPositive}></Checkbox>
+                      <Checkbox label="Pos"  onChange={this.handleCardSentimentNegative}></Checkbox>
                         {
                         this.state.data.length === 0 ? (<div></div>):
                             (<MyTweets cardSentimentPositive = {this.state.cardSentimentPositive} cardSentimentNegative = {this.state.cardSentimentNegative} data = {this.state.data} insurance={this.state.insur} card={this.state.card}  col = "left"/>)
                         }
                     </Grid.Column>
                     <Grid.Column>
-                          <h3>Insurance Tweets</h3>
-                          <Checkbox label="Remove Positive Sentiments" onChange={this.handleInsuranceSentimentPositive}></Checkbox>
-                          <Checkbox label="Remove Negative Sentiments" onChange={this.handleInsuranceSentimentNegative}></Checkbox>
+                          <h3>Customer Related Messages</h3>
+                          <Checkbox label="Neg" style={{marginRight:"10px"}} onChange={this.handleInsuranceSentimentPositive}></Checkbox>
+                          <Checkbox label="Pos" onChange={this.handleInsuranceSentimentNegative}></Checkbox>
                             {
                               this.state.data.length === 0 ? (<div></div>):
                             (<MyTweets data = {this.state.data} insurancePositiveSentiment = {this.state.insuranceSentimentPositive} insuranceNegativeSentiment = {this.state.insuranceSentimentNegative} insurance={this.state.insur} card={this.state.card} col = "right"/>)
@@ -722,14 +722,14 @@ class RenderComp extends Component {
                 </Grid.Row>
                 <Grid.Row>
                     <Grid.Column width={8}>
-                        <h3>Links</h3>
+                        <h3>Landing Pages</h3>
                         {
                         this.state.data.length === 0 ? (<div></div>):
                         <Mylinks data = {this.state.data} insurance={this.state.insur} card={this.state.card} col = "left"/>
                         }
                     </Grid.Column>
                     <Grid.Column>
-                        <h3>Links</h3>
+                        <h3>Landing Pages</h3>
                         {
                         this.state.data.length === 0 ? (<div></div>):
                         <Mylinks data = {this.state.data} insurance={this.state.insur} card={this.state.card} col = "right"/>
