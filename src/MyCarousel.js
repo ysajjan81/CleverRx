4ecdb0e9936e7828263d5b48f7ccdb4c4b35d6ab
Keerthivasan Krishnamurthy <keerthivasankrishnamurthy@Keerthivasans-MacBook-Pro.local>
2020-03-20 18:13:59 -0700

    Added dynamic rendering of caurosal

--- src/MyCarousel.js
+++ src/MyCarousel.js
@@ -1,14 +1,49 @@
 import React, { Component } from 'react';
 import {Carousel} from 'react-bootstrap';
 // import './assets/css/App.css';
+
 class MyCarousel extends Component {
     constructor(props) {
         super(props);
         this.state = { 
          }
     }
-    render() { 
-      let myList = [];
+
+    renderCarousel(imgList){
+      if(imgList !== null && imgList.length > 0){
+        return imgList.map(function(image, index){
+          return(
+            <Carousel.Item >
+              <img
+                src={image.imgUrl}
+                alt={image.imgTitle}
+                style={{width:'100%', height:'370px'}}
+              />
+              <Carousel.Caption>
+                <h3>{image.imgTitle}</h3>
+                <p>{image.imgDesc}</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          );
+        })
+      }
+    }
+    render(){
+      let imgList = [{
+        imgUrl:"https://image.shutterstock.com/image-photo/hands-holding-credit-card-using-600w-289585190.jpg",
+        imgTitle:"First",
+        imgDesc:"First Img Desc"
+      },{
+        imgUrl:"https://image.shutterstock.com/image-photo/hands-holding-credit-card-using-600w-289585190.jpg",
+        imgTitle:"Second",
+        imgDesc:"Second Img Desc"
+      },
+      {
+        imgUrl:"https://image.shutterstock.com/image-photo/hands-holding-credit-card-using-600w-289585190.jpg",
+        imgTitle:"Third",
+        imgDesc:"Third Img Desc"
+      }
+    ];
 
       // if(this.props.data && this.props.data.length != 0 && value == "insurance")
       // {
@@ -44,107 +79,16 @@ class MyCarousel extends Component {
       //   }
       // }
 
-        return (
+      return(
         <div>
-            <Carousel >
-            <Carousel.Item >
-    <img style={{ height: '370px'}}
-    //   className="d-block w-100"
-      src="https://image.shutterstock.com/image-photo/hands-holding-credit-card-using-600w-289585190.jpg"
-      alt="First slide"
-    />
-    
-    <Carousel.Caption>
-      <h3>First slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img style={{ height: '370px'}}
-    // className="d-block w-100"
-      src="https://image.shutterstock.com/image-photo/hands-holding-credit-card-using-600w-289585190.jpg"
-      alt="Third slide"
-
-    />
-    <Carousel.Caption>
-      <h3>Second slide label</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img style={{ height: '370px'}}
-        // className="d-block w-100"
-      src="https://image.shutterstock.com/image-photo/hands-holding-credit-card-using-600w-289585190.jpg"
-      alt="Third slide"
-    />
-    <Carousel.Caption>
-      <h3>Third slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-</Carousel>
-</div> );
-    }
+            <Carousel>
+              {
+                this.renderCarousel(imgList)
+              }
+          </Carousel>
+        </div> 
+      );
+  }
 }
 
 export default MyCarousel;
-
-// import React, { Component } from 'react';
-// import Slider from 'react-slick';
-// // import "~slick-carousel/slick/slick.css"; 
-// // import "~slick-carousel/slick/slick-theme.css";
-
-// const photos = [
-//     {
-//         name: "Photo 1",
-//         url: "https://image.shutterstock.com/image-photo/hands-holding-credit-card-using-600w-289585190.jpg"
-//     },
-//     {
-//         name: "Photo 2",
-//         url: "https://image.shutterstock.com/image-photo/hands-holding-credit-card-using-600w-289585190.jpg"
-//     },
-//     {
-//         name: "Photo 3",
-//         url: "https://image.shutterstock.com/image-photo/hands-holding-credit-card-using-600w-289585190.jpg"
-//     }
-
-// ]
-
-// class MyCarousel extends Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {  }
-//     }
-//     render() { 
-//         const settings = {
-//             dots: true,
-//             fade: true,
-//             infinite: true,
-//             speed: 500,
-//             slidesToShow: 1,
-//             arrows: true,
-//             slidesToScroll: 1,
-//             className: "slides",
-
-//         }
-//         return ( 
-//             <div>
-//                 <Slider {...settings}>
-//                     {
-//                         photos.map((photo) => 
-//                         {
-//                             return (
-//                                 <div>
-//                                     <img width = "100%" src={photo.url}></img>
-//                                 </div>
-//                             )
-//                         }
-//                         )
-//                     }
-//                 </Slider>
-//             </div>
-//          );
-//     }
-// }
- 
-// export default MyCarousel;
